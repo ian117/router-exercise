405b614e2635ee224fe984f98357cae2b79313c6
ian117 <spideramerica71@gmail.com>
2021-02-22 20:55:04 -0600

    lets see

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import {
-    BrowserRouter as Router,
     Switch,
     Route,
-    Link,
-    BrowserRouter
   } from "react-router-dom";
 import About from "./About";
 import FAQ from "./FAQ";
